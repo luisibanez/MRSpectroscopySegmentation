6bee9197a5f30570840699115f8835082b983523
Luis Ibanez <luis.ibanez@kitware.com>
2012-02-28 13:24:26 -0500

    ENH: First cut at reading MR Spectroscopy data.

---
+++ src/DicomImageReadWrite.cxx
@@ -0,0 +1,49 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "gdcmReader.h"
+#include "gdcmAttribute.h"
+
+int main( int argc, char* argv[] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " DicomImage OutputImage" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  gdcm::Reader reader;
+
+  reader.SetFileName( argv[1] );
+  reader.Read();
+
+  const gdcm::File & file = reader.GetFile();
+
+  const gdcm::DataSet & dataset = file.GetDataSet();
+
+  gdcm::Attribute<0x5600,0x0020> at;
+  at.Set( dataset );
+
+  std::cout << at.GetValue() << std::endl;
+
+
+  return EXIT_SUCCESS;
+
+}
